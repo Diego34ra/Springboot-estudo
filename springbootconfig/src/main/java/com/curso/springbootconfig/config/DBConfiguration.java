cf0afb1e71be4630d6cb148f3ec16cfd75deb267
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-10 12:58:00 -0300

    Atualizando o DBConfiguration
    Finalizando a configuração do banco de dados.

--- springbootconfig/src/main/java/com/curso/springbootconfig/config/DBConfiguration.java
+++ springbootconfig/src/main/java/com/curso/springbootconfig/config/DBConfiguration.java
@@ -1,13 +1,17 @@
 package com.curso.springbootconfig.config;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @ConfigurationProperties("spring.datasource")
-
+@Getter
+@Setter
 public class DBConfiguration {
 
 
@@ -16,11 +20,22 @@ public class DBConfiguration {
     private String username;
     private String password;
 
+    @Profile("dev")
+    @Bean
     public String testeDataBaseConnection(){
         System.out.println("DB connection for DEV - H2");
         System.out.println(driverClassName);
         System.out.println(url);
         return "DB Connection to H2_TEST - Teste instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String prodDataBaseConnection(){
+        System.out.println("DB connection for PROD - MySQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+        return "DB Connection to MySQL - Teste production";
     }
 
 }
